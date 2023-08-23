37c164e37e6c2596ca8b5087254ce81a3526d748
EL Putnam <elputnam@inaction.ie>
2023-08-23 16:47:10 +0100

    change framerate

--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function setup() {
   createCanvas(base.width, base.height);
   colorMode(HSB, 360, 100, 100, 100);
   background(0);
-  frameRate(10);
+  frameRate(7);
   rectMode(CENTER);
   // Array of shadows
   for (let k = 1; k < sil.length; k++){
